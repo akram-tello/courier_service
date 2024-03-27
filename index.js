7001500f766139592670432a9105a83d0b185478
akram-tello <a.tello@graduate.utm.my>
2024-03-28 06:39:46 +0800

    update index to select operation

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-import { getBaseDeliveryCostAndPackages } from './src/utils/inputHandler.js';
-import { calculateDeliveryCost } from './src/services/deliveryCostCalculator.js';
+import inquirer from 'inquirer';
 import chalk from 'chalk';
+import { getBaseDeliveryCostAndPackages, getVehicleInfo } from './src/utils/inputHandler.js';
+import { calculateDeliveryCost } from './src/services/deliveryCostCalculator.js';
+import { DeliveryTimeCalculator } from './src/services/deliveryTimeCalculator.js';
+
+async function mainMenu() {
+  console.log(chalk.blue("\nWelcome to Kiki's Courier Service!"));
+  const { choice } = await inquirer.prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'Select an operation:',
+    choices: ['Delivery Cost Estimation', 'Estimated Delivery Time', 'Exit'],
+  });
+
+  switch (choice) {
+    case 'Delivery Cost Estimation':
+      await processPackages();
+      break;
+    case 'Estimated Delivery Time':
+      await processDeliveryTime();
+      break;
+    case 'Exit':
+      console.log(chalk.green('Thank you for using Courier Service CLI.'));
+      break;
+  }
+}
 
 async function processPackages() {
   const { baseDeliveryCost, packages } = await getBaseDeliveryCostAndPackages();
-
   console.log(chalk.green('\nCalculated Delivery Costs:'));
   console.log(chalk.green('-----------------------------------------------'));
   console.log(chalk.green('| ID    | Discount ($) | Total Cost ($)       |'));
@@ -22,10 +45,20 @@ async function processPackages() {
       if (message) {
           console.log(chalk.yellow(message)); // Display any specific message regarding offer code
       }
-  });
-
-  console.log(chalk.green('-----------------------------------------------'));
+    });
 }
 
+async function processDeliveryTime() {
+  const { baseDeliveryCost, packages } = await getBaseDeliveryCostAndPackages();
+  const vehicleInfo = await getVehicleInfo();
+  const results = DeliveryTimeCalculator({ baseDeliveryCost, packages, vehicleInfo });
 
-processPackages().catch(err => console.error(chalk.red(err.message)));
+  console.log(chalk.green('\nEstimated Delivery Times:'));
+  results.forEach(result => {
+    console.log(`${result.pkg_id} ${result.discount} ${result.total_cost} ${result.estimated_delivery_time}`);
+  });
+}
+
+mainMenu().catch(err => {
+  console.error(chalk.red(`Error: ${err.message}`));
+});
