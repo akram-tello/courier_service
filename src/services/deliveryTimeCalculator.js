a2e6f1df83a6d2fee88fbecb2b06c631ee120cc0
akram-tello <a.tello@graduate.utm.my>
2024-03-28 06:36:37 +0800

    add DeliveryTimeCalculator

--- src/services/deliveryTimeCalculator.js
+++ src/services/deliveryTimeCalculator.js
@@ -1,1 +1,29 @@
-// New service for delivery time estimation
+export class DeliveryTimeCalculator {
+    constructor(packages, vehicleInfo) {
+        this.packages = packages; 
+        this.vehicleInfo = vehicleInfo; // { noOfVehicles, maxSpeed, maxCarriableWeight }
+    }
+
+    calculate() {
+        // Sort packages by weight descendingly to prioritize heavier packages for delivery
+        const sortedPackages = this.packages.sort((a, b) => b.weight - a.weight || b.distance - a.distance);
+
+        const deliveryTimes = [];
+        const vehicleAvailability = new Array(this.vehicleInfo.noOfVehicles).fill(0); // Track when each vehicle will be available next
+
+        sortedPackages.forEach(pkg => {
+            const earliestAvailableVehicleIndex = vehicleAvailability.indexOf(Math.min(...vehicleAvailability));
+            const travelTime = pkg.distance / this.vehicleInfo.maxSpeed;
+            const totalTime = vehicleAvailability[earliestAvailableVehicleIndex] + travelTime * 2; // Considering round trip
+
+            vehicleAvailability[earliestAvailableVehicleIndex] = totalTime; // Update vehicle's next available time
+
+            deliveryTimes.push({
+                pkg_id: pkg.id,
+                estimated_delivery_time: totalTime.toFixed(2),
+            });
+        });
+
+        return deliveryTimes;
+    }
+}
