0edf43052b07c15575c05cf05c2391679e647718
akram-tello <a.tello@graduate.utm.my>
2024-04-04 02:15:29 +0800

    update deliveryTimeCalculator logic

--- src/services/deliveryTimeCalculator.js
+++ src/services/deliveryTimeCalculator.js
@@ -1,29 +1,36 @@
 export class DeliveryTimeCalculator {
     constructor(packages, vehicleInfo) {
-        this.packages = packages; 
-        this.vehicleInfo = vehicleInfo; // { noOfVehicles, maxSpeed, maxCarriableWeight }
+        this.packages = packages;
+        this.vehicleInfo = vehicleInfo;
     }
 
     calculate() {
-        // Sort packages by weight descendingly to prioritize heavier packages for delivery
-        const sortedPackages = this.packages.sort((a, b) => b.weight - a.weight || b.distance - a.distance);
-
-        const deliveryTimes = [];
-        const vehicleAvailability = new Array(this.vehicleInfo.noOfVehicles).fill(0); // Track when each vehicle will be available next
+        const sortedPackages = this.packages.sort((a, b) => b.weight - a.weight);
+        let vehicleAvailability = new Array(this.vehicleInfo.noOfVehicles).fill(0);
+        let deliveryTimes = [];
 
         sortedPackages.forEach(pkg => {
-            const earliestAvailableVehicleIndex = vehicleAvailability.indexOf(Math.min(...vehicleAvailability));
-            const travelTime = pkg.distance / this.vehicleInfo.maxSpeed;
-            const totalTime = vehicleAvailability[earliestAvailableVehicleIndex] + travelTime * 2; // Considering round trip
+            let vehicleIndex = vehicleAvailability.findIndex(time => time <= vehicleAvailability.reduce((a, b) => Math.min(a, b)));
+            let maxLoad = this.vehicleInfo.maxCarriableWeight;
+            let load = 0;
+            let farthestDistance = 0;
 
-            vehicleAvailability[earliestAvailableVehicleIndex] = totalTime; // Update vehicle's next available time
-
-            deliveryTimes.push({
-                pkg_id: pkg.id,
-                estimated_delivery_time: totalTime.toFixed(2),
-            });
+            for (let i = 0; i < sortedPackages.length && load + sortedPackages[i].weight <= maxLoad; i++) {
+                if (!sortedPackages[i].assigned) {
+                    load += sortedPackages[i].weight;
+                    farthestDistance = Math.max(farthestDistance, sortedPackages[i].distance);
+                    sortedPackages[i].assigned = true;
+                    let travelTime = (farthestDistance / this.vehicleInfo.maxSpeed) * 2;
+                    let deliveryTime = vehicleAvailability[vehicleIndex] + travelTime;
+                    deliveryTimes.push({
+                        pkg_id: sortedPackages[i].id,
+                        estimated_delivery_time: parseFloat(deliveryTime.toFixed(2))
+                    });
+                }
+            }
+            vehicleAvailability[vehicleIndex] += (farthestDistance / this.vehicleInfo.maxSpeed) * 2;
         });
 
-        return deliveryTimes;
+        return deliveryTimes.sort((a, b) => a.pkg_id.localeCompare(b.pkg_id));
     }
 }
